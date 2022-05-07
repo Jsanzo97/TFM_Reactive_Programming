1c2cb1016fd8683b6824a7735518529c1f5c2d41
Jsanzo97 <jorgesanzo21@gmail.com>
2022-05-07 16:49:12 +0200

    reorganized functionallity

--- app/src/main/kotlin/com/example/reactiveprogramming/di/home/HomeModule.kt
+++ app/src/main/kotlin/com/example/reactiveprogramming/di/home/HomeModule.kt
@@ -2,15 +2,11 @@ package com.example.reactiveprogramming.di.home
 
 import com.example.common.UiConfigurationViewModel
 import com.example.domain.usecase.InitializeDatabaseUseCase
-import com.example.reactiveprogramming.ui.home.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val homeModule = module {
 
-    viewModel { HomeViewModel(get()) }
     viewModel { UiConfigurationViewModel() }
-
-    factory { InitializeDatabaseUseCase(get()) }
 
 }
--- app/src/main/kotlin/com/example/reactiveprogramming/ui/home/HomeViewState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.reactiveprogramming.ui.home
-
-sealed class HomeViewState
-
-object InitializingDatabase: HomeViewState()
-object InitializedDatabase: HomeViewState()
-class ErrorInOperation(val message: String): HomeViewState()
