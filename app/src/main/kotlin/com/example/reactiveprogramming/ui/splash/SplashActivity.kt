3288b7eebe75de7ded8dc69c840efef36191dea6
Jsanzo97 <jorgesanzo21@gmail.com>
2022-05-19 16:22:42 +0200

    Added more documentation and refactor code

--- app/src/main/kotlin/com/example/reactiveprogramming/ui/splash/SplashActivity.kt
+++ app/src/main/kotlin/com/example/reactiveprogramming/ui/splash/SplashActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.reactiveprogramming.ui.home.HomeActivity
 
+/**
+ * Show splash view during the application loading, this is manage in the manifest setting the theme for this activity,
+ * then start the home activity. By this way we show something to user who is waiting the application initialization
+ */
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
