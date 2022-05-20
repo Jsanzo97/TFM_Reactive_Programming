4bca9085a9dc4d2b7d41120ec723a83a00a35ae6
Jsanzo97 <jorgesanzo21@gmail.com>
2022-05-20 17:16:30 +0200

    Added documentation

--- common/src/main/kotlin/com/example/common/extensions/ContextExtensions.kt
+++ common/src/main/kotlin/com/example/common/extensions/ContextExtensions.kt
@@ -9,6 +9,10 @@ import androidx.annotation.StyleRes
 import androidx.core.content.res.ResourcesCompat
 import arrow.core.Option
 
+/**
+ * Obtains the action bar size from system
+ * @return size of the action bar
+ */
 fun Context.getActionBarSize(): Int {
     val tv = TypedValue()
     return if (theme.resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
@@ -18,10 +22,22 @@ fun Context.getActionBarSize(): Int {
     }
 }
 
+/**
+ * Obtains color from compat resources
+ * @param colorId resource color id
+ * @return color from compar resources
+ */
 @ColorInt
 fun Context.getColorCompat(@ColorRes colorId: Int) =
     ResourcesCompat.getColor(resources, colorId, null)
 
+/**
+ * Obtains color from style
+ * @param style style id to get the color
+ * @param attr attribute of the style to get the color
+ * @param defaultColor color id to return if something fails
+ * @return color from style
+ */
 @ColorInt
 fun Context.getColorFromStyle(@StyleRes style: Int, @AttrRes attr: Int, @ColorInt defaultColor: Int): Int {
     val ta = obtainStyledAttributes(style, intArrayOf(attr))
@@ -30,5 +46,17 @@ fun Context.getColorFromStyle(@StyleRes style: Int, @AttrRes attr: Int, @ColorIn
     return themeColor
 }
 
+/**
+ * Obtains color or return default color if not exists
+ * @param optionColor optional color to get
+ * @param colorInt default color
+ */
 fun Context.getColorOrDefault(optionColor: Option<Int>, @ColorInt colorInt: Int) =
-    optionColor.fold({ colorInt }, { getColorCompat(it) })
+    optionColor.fold(
+        {
+            colorInt
+        },
+        {
+            getColorCompat(it)
+        }
+    )
--- common/src/main/kotlin/com/example/common/fragment/CustomFragment.kt
+++ common/src/main/kotlin/com/example/common/fragment/CustomFragment.kt
@@ -10,15 +10,22 @@ import com.example.common.DIALOG_FRAGMENT_TAG
 import com.example.common.R
 import com.example.common.UiConfigurationViewModel
 import com.example.common.UiConfigurationViewState
-import com.example.common.extensions.hideKeyboard
 import com.google.android.material.textview.MaterialTextView
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
+/**
+ * Base fragment to have some common functions and code in all fragments of the application
+ * @param contentLayoutId layout to bind with the fragment to paint it on the screen
+ */
 abstract class CustomFragment(private val contentLayoutId: Int) : Fragment() {
 
+    // ViewModel to manage ui configuration changes
     private val uiConfigurationViewModel by sharedViewModel<UiConfigurationViewModel>()
+
+    // UI Configuration for custom toolbar, fragments should override this property
     protected abstract val uiConfigurationViewState: UiConfigurationViewState
 
+    // Common custom views for fragments
     private lateinit var progressView: View
     private lateinit var progressDialog: AlertDialog
 
@@ -32,8 +39,11 @@ abstract class CustomFragment(private val contentLayoutId: Int) : Fragment() {
         return inflater.inflate(contentLayoutId, container, false)
     }
 
+    /**
+     * Shows error message on the screen
+     * @param message description of the error
+     */
     fun showError(message: String) {
-        hideKeyboard()
         hideProgressDialog()
         CustomDialogFragment(
             message = message,
@@ -41,6 +51,10 @@ abstract class CustomFragment(private val contentLayoutId: Int) : Fragment() {
         ).show(requireFragmentManager(), DIALOG_FRAGMENT_TAG)
     }
 
+    /**
+     * Shows progress dialog on the screen
+     * @param title title of the progress dialog
+     */
     fun showProgressDialog(title: String = getString(R.string.loading)) {
         if (!::progressDialog.isInitialized) {
             progressView = layoutInflater.inflate(R.layout.custom_progress_dialog, null)
@@ -54,12 +68,19 @@ abstract class CustomFragment(private val contentLayoutId: Int) : Fragment() {
         progressDialog.show()
     }
 
+    /**
+     * Changes the progress dialog title
+     * @param title new title for the progress dialog
+     */
     fun updateProgressDialogTitle(title: String) {
         if (::progressDialog.isInitialized) {
             progressView.findViewById<MaterialTextView>(R.id.title_progress_dialog).text = title
         }
     }
 
+    /**
+     * Hides the progress dialog
+     */
     fun hideProgressDialog() {
         if (::progressDialog.isInitialized) {
             progressDialog.dismiss()
