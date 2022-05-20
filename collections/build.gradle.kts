67149af52bdb1790c0abe1839f1551fc4ce97d2e
Jsanzo97 <jorgesanzo21@gmail.com>
2022-05-20 22:16:49 +0200

    Clean code

--- collections/build.gradle.kts
+++ collections/build.gradle.kts
@@ -1,33 +1,11 @@
 plugins {
-    id(Plugins.androidLibrary)
-    id(Plugins.kotlinAndroid)
+    id(Plugins.javaLibrary)
+    id(Plugins.kotlin)
 }
 
-android {
-    compileSdkVersion(Versions.targetSdk)
-
-    defaultConfig {
-        minSdkVersion(Versions.minSdk)
-        targetSdkVersion(Versions.targetSdk)
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
-        }
-    }
-
-    sourceSets {
-        forEach {
-            it.java.srcDir("src/${it.name}/kotlin")
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = Versions.sourceCompatibility
-        targetCompatibility = Versions.targetCompatibility
-    }
+configure<JavaPluginExtension> {
+    sourceCompatibility = Versions.sourceCompatibility
+    targetCompatibility = Versions.targetCompatibility
 }
 
 dependencies {
